b58be2f8ee11da6c0a1f5605eb97a36d8291647d
Jason-MacDonald <49774612+Jason-MacDonald@users.noreply.github.com>
2021-01-24 15:59:39 +1300

    education achievements changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+const renderAchievements = (achievements) => {
+  return achievements.map((x) => {
+    return (
+      <h2>
+        <i>
+          - {x.achievement}: {x.year}
+        </i>
+      </h2>
+    );
+  });
+};
 
 class Resume extends Component {
-    render() {
-        return (
-            <div id="resume">
-                <div className="ui grid container" style={{ paddingTop: '200px', paddingBottom: '200px' }}>
-                    <div className="row">
-                        <div className="four wide column">
-                            <h1 className="ui header" style={{ borderBottom: 'teal solid'}}>
-                                <i className="graduation cap icon"></i>
-                                Education
-                            </h1>
-                        </div>
-                        <div className="one wide column" />
-                        <div className="eleven wide column">
-                            <h1>{ this.props.resumeData.education[0].schoolName }</h1>
-                            <h2><i>{this.props.resumeData.education[0].major} - { this.props.resumeData.education[0].graduationYear}</i></h2>
-                            <h1>Achievements</h1>
-                        </div>
-                    </div>
-                </div>
-                {/* <div className="ui grid container" style={{ marginTop: '100px' }}>
+  render() {
+    const { achievements } = this.props.resumeData.education[0];
+    return (
+      <div id="resume">
+        <div
+          className="ui grid container"
+          style={{ paddingTop: "200px", paddingBottom: "200px" }}
+        >
+          <div className="row">
+            <div className="four wide column">
+              <h1 className="ui header" style={{ borderBottom: "teal solid" }}>
+                <i className="graduation cap icon"></i>
+                Education
+              </h1>
+            </div>
+            <div className="one wide column" />
+            <div className="eleven wide column">
+              <h1>{this.props.resumeData.education[0].schoolName}</h1>
+              <h2>
+                <i>
+                  {this.props.resumeData.education[0].major} -{" "}
+                  {this.props.resumeData.education[0].graduationYear}
+                </i>
+              </h2>
+              <h1>Achievements</h1>
+              {achievements && renderAchievements(achievements)}
+            </div>
+          </div>
+        </div>
+        {/* <div className="ui grid container" style={{ marginTop: '100px' }}>
                     <div className="row">
                         <div className="four wide column">
                             <h1 className="ui header" style={{ borderBottom: 'teal solid'}}>
@@ -36,9 +58,9 @@ class Resume extends Component {
                         </div>
                     </div>
                 </div> */}
-            </div>
-        )
-    };
-};
+      </div>
+    );
+  }
+}
 
 export default Resume;
